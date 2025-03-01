2849cd9d35fc4189df9d4cc9b4a936949629f897
Suvijak Phuphakdeepan <suvijak237@gmail.com>
2025-03-01 19:10:54 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
+const cookieParser = require('cookie-parser');
 const connectDB = require('./config/db');
 
 //load env vars
@@ -9,12 +9,22 @@ dotenv.config({ path: './config/config.env' });
 //Connect to database
 connectDB();
 
+//Route files
+const coworkingspaces = require('./routes/coworkingspaces');
+const auth = require('./routes/auth');
+const reservations = require('./routes/reservations');
+
 const app = express();
 
 //body parser
 app.use(express.json());
 
-//Route files
+app.use('/api/v1/coworkingspaces', coworkingspaces);
+app.use('/api/v1/auth', auth);
+app.use('/api/v1/reservations', reservations);
+
+//cookie parser
+app.use(cookieParser());
 
 const PORT = process.env.PORT || 5000;
 
