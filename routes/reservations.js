f480957f621a6862fd548a034a4086eaf7250346
Suvijak Phuphakdeepan <suvijak237@gmail.com>
2025-03-03 12:40:20 +0700

    Fix routes

--- routes/reservations.js
+++ routes/reservations.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const {getReservations, getReservation, addReservation, updateReservation,deleteReservation} = require('../controllers/reservations');
+const { getReservations, getReservation, addReservation, updateReservation, deleteReservation, searchReservationsByTime } = require('../controllers/reservations');
 
-const router = express.Router({mergeParams: true});
+const router = express.Router({ mergeParams: true });
 
-const { protect,authorize } = require('../middleware/auth');
+const { protect, authorize } = require('../middleware/auth');
 
-router.route('/').get(protect, getReservations).post(protect,authorize('user','admin'), addReservation);
-router.route('/:id').get(protect,getReservation).put(protect,authorize('user','admin'),updateReservation).delete(protect,authorize('user','admin'),deleteReservation);
+router.route('/').get(protect, getReservations).post(protect, authorize('user', 'admin'), addReservation);
+router.route('/search').get(protect, searchReservationsByTime); // Search reservations by time
+router.route('/:id').get(protect, getReservation).put(protect, authorize('user', 'admin'), updateReservation).delete(protect, authorize('user', 'admin'), deleteReservation);
 
 module.exports = router;
