5ad8f903992c28aa305ab1d4d3b5af722b8b4e2c
watthanakorn Rakthong <watthanakorn.r@gmail.com>
2025-03-22 22:38:07 +0700

    update

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -7,7 +7,7 @@ const sendTokenResponse = (user, statusCode, res) => {
     const token = user.getSignedJwtToken();
 
     const option = {
-        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
+        expires: new Date(Date.now() + Number(process.env.JWT_COOKIE_EXPIRE) * 24 * 60 * 60 * 1000),
         httpOnly: true
     };
 
